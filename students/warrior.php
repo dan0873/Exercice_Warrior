fa02c74c721bf01b855311cac25311f22004514b
Dan0873 <danilo.quatraccioni@le-campus-numerique.fr>
2021-08-27 12:23:02 +0200

    exo_etape_7

--- students/warrior.php
+++ students/warrior.php
@@ -14,7 +14,9 @@ abstract class Warrior extends LocalWarrior {
     public $shield;
     public $imageUrl;
     public $weapon;
-
+    
+    public function __construct(){}
+    
 };
 
 // class StartrekWarrior{} //création classe StartrekWarrior
@@ -27,29 +29,51 @@ abstract class Warrior extends LocalWarrior {
 // faire hériter la classe de Warrior
 class StartrekWarrior extends Warrior {
     public $mentalPower;
+    public function __construct(){}
 
     public function power() {
       return $this->mentalPower;
     }
 
+    
+
 }
+
 
 class MarvelWarrior extends Warrior{
     public $superPower;
+    public function __construct(){}
 
     public function power() {
         return $this->superPower;
     }  
+
+    
 } 
+
 
 class PokemonWarrior extends Warrior{
     public $level;
+    public function __construct(){}
     
     public function power() {
         return $this->level;
         
     }
+
+    
 }
+
+
+// public function __construct(int $id, string $name, int $speed, int $life, int $shield, string $imageUrl, string $weapon ){
+//     $this->id=$id;
+//     $this->name=$name;
+//     $this->speed=$speed;
+//     $this->life=$life;
+//     $this->shield=$shield;
+//     $this->imageUrl=$imageUrl;
+//     $this->weapon=$weapon;  
+// }
 
 
 
