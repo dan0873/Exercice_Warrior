a5dcd0b4996f940f2b2ff64270b16f65d6d7627f
Dan0873 <danilo.quatraccioni@le-campus-numerique.fr>
2021-08-27 11:44:56 +0200

    exo_etape_6

--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,64 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'CNA_Danilo'; //modifier la variable globale
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
 
 };
+
+// class StartrekWarrior{} //création classe StartrekWarrior
+
+// class MarvelWarrior{} //création classe MarvelWarrior
+
+// class PokemonWarrior{}//création classe PokemonWarrior
+
+
+// faire hériter la classe de Warrior
+class StartrekWarrior extends Warrior {
+    public $mentalPower;
+
+    public function power() {
+      return $this->mentalPower;
+    }
+
+}
+
+class MarvelWarrior extends Warrior{
+    public $superPower;
+
+    public function power() {
+        return $this->superPower;
+    }  
+} 
+
+class PokemonWarrior extends Warrior{
+    public $level;
+    
+    public function power() {
+        return $this->level;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
